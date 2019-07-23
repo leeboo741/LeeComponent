6a12bd8d4546ed4123725dd716853781d6c26e68
Jingbo@li <18811145767@163.com>
2019-07-23 15:25:37 +0800

    Update calendar.js

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    lightingDayList:{}, // 点亮的时间
+    lightingDayList:[], // 点亮的时间
   },
 
   /**
@@ -63,56 +63,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let tempDate1 = timeUtils.getDate(new Date(), 0, 0, 0, 1, 0, 0, 0);
-    let tempDate2 = timeUtils.getDate(new Date(), 0, 0, 1, 0, 0, 0, 0);
-    let tempDate3 = timeUtils.getDate(new Date(), 0, 0, 0, 3, 0, 0, 0);
-    let tempDate4 = timeUtils.getDate(new Date(), 0, 0, 0, 5, 0, 0, 0);
-    let tempDate5 = timeUtils.getDate(new Date(), 0, 1, 0, 0, 0, 0, 0);
-    let tempDate6 = timeUtils.getDate(new Date(), 0, 0, 0, 8, 0, 0, 0);
-
-    // warning 红色 | alert 黄色 | remind 蓝色 | complete 绿色 
-    let tempData1 = {
-      day: tempDate1.getDate(),
-      month: tempDate1.getMonth() + 1,
-      year: tempDate1.getFullYear(),
-      lightingType: "warning",
-    };
-    let tempData2 = {
-      day: tempDate2.getDate(),
-      month: tempDate2.getMonth() + 1,
-      year: tempDate2.getFullYear(),
-      lightingType: "default",
-    };
-    let tempData3 = {
-      day: tempDate3.getDate(),
-      month: tempDate3.getMonth() + 1,
-      year: tempDate3.getFullYear(),
-      lightingType: "remind",
-    };
-    let tempData4 = {
-      day: tempDate4.getDate(),
-      month: tempDate4.getMonth() + 1,
-      year: tempDate4.getFullYear(),
-      lightingType: "complete",
-    };
-    let tempData5 = {
-      day: tempDate5.getDate(),
-      month: tempDate5.getMonth() + 1,
-      year: tempDate5.getFullYear(),
-      lightingType: "alert",
-    };
-    let tempData6 = {
-      day: tempDate6.getDate(),
-      month: tempDate6.getMonth() + 1,
-      year: tempDate6.getFullYear(),
-      lightingType: "alert",
-    };
-    let tempList = [];
-    tempList = [tempData1, tempData2, tempData3, tempData4, tempData5, tempData6];
-
-    this.setData({
-      lightingDayList: tempList
-    })
+    
   },
 
   /**
