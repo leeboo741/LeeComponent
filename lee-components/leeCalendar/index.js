9b2294b423cd3d0afe734653b77ff6c70d779cd9
Jingbo@li <18811145767@163.com>
2019-07-23 14:14:44 +0800

    201907231413
    1 日历功能基本完成
    2 可以选中|取消时间选中
    3 月份选择
    4 几种选中模式

--- lee-components/leeCalendar/index.js
+++ lee-components/leeCalendar/index.js
@@ -14,32 +14,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    haveContentTimeList:{
+    lightingDayList:{
       type: Array,
       value: [],
-    }, // 选中的时间列表
+    }
   },
 
   /**
    * 组件的初始数据 内部属性
    */
   data: {
-    currentDate: null, // 当前时间
-    weeks: [
-      "周日",
-      "周一",
-      "周二",
-      "周三",
-      "周四",
-      "周五",
-      "周六"
-    ],
+    currentPageDate: null, // 当前页时间
+    currentPageTitle: null, // 当前页月份
+    today: null, // 今天
+    weeks: ["周日","周一","周二","周三","周四","周五","周六"], // 星期
+    dayDataSource: [], // 日期数据
   },
 
   /**
    * 属性监听
    */
   observers: {
+    "lightingDayList": function (lightingDayList) {
+      this._getDaySource(this.data.currentPageDate);
+    }
   },
 
   /** ==================================================== */
@@ -54,12 +52,98 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    // 获取 开始 和 结束 的星期
-    _getStartAndEndDayWeek: function () {
-      let startWeek = timeUtils.getStartDayForMonth(this.data.currentDate).getDay();
-      let endWeek = timeUtils.getEndDayForMonth(this.data.currentDate).getDay();
-      let dayCount = timeUtils.getDayCountForMonth(this.data.currentDate);
+
+    // 获取 当月 的 日期数据
+    _getDaySource: function (date) {
+      // 当月 月份
+      let tempCurrentPageMonth = date.getMonth() + 1;
+      this.setData({
+        currentPageTitle: timeUtils.dateFormat(date,"yyyy年MM月")
+      })
+
+      let startWeek = timeUtils.getStartDayForMonth(date).getDay();
+      let endWeek = timeUtils.getEndDayForMonth(date).getDay();
+      let dayCount = timeUtils.getDayCountForMonth(date);
+      let tempDaySource = [];
+      // 插入 1号 之前 的空数据
+      for (let i = 0; i < startWeek; i++) {
+        tempDaySource.push(null);
+      }
+      // 插入 日期数据
+      for (let i = 0; i < dayCount; i++) {
+        let dayData = {};
+        let tempDate = new Date(date);
+        tempDate = tempDate.setDate(i + 1);
+        tempDate = new Date(tempDate);
+        dayData.date = tempDate;
+        dayData.day = tempDate.getDate();
+        dayData.month = tempDate.getMonth() + 1;
+        dayData.year = tempDate.getFullYear();
+        dayData.weeks = tempDate.getDay(); 
+        if (tempDate.getDate() == this.data.today.getDate()
+          && tempDate.getFullYear() == this.data.today.getFullYear()
+          && tempDate.getMonth() == this.data.today.getMonth()) {
+          dayData.lightingType = "today";
+        } else {
+          dayData.lightingType = "default";
+          if (this.properties.lightingDayList != null
+            && this.properties.lightingDayList.length > 0) {
+            for (let j = 0; j < this.properties.lightingDayList.length; j++) {
+              let tempLightingItem = this.properties.lightingDayList[j];
+              if (tempLightingItem.day == dayData.day
+                && tempLightingItem.month == dayData.month
+                && tempLightingItem.year == dayData.year) {
+                dayData.lightingType = tempLightingItem.lightingType;
+                break;
+              }
+            }
+          }
+        }
+        tempDaySource.push(dayData);
+      }
+      // 插入 本月结束后 的空数据
+      for (let i = endWeek; i < 6; i++) {
+        tempDaySource.push(null);
+      }
+      console.log(JSON.stringify(tempDaySource));
+      this.setData({
+        dayDataSource: tempDaySource
+      })
     },
+
+    // 当前页时间
+    _setCurrentPageDate: function (monthDuration) {
+      let that = this;
+      let tempCurrentPageDate = timeUtils.getDate(this.data.currentPageDate, 0, monthDuration, 0, 0, 0, 0, 0);
+      this.setData({
+        currentPageDate: tempCurrentPageDate,
+      })
+      this._getDaySource(this.data.currentPageDate);
+    } ,
+
+    // 点击日期
+    tapDayItem: function (e) {
+      let callBackFunctionName = 'calendar-tapday'; // 触发事件 方法名
+      let myEventDetail = e; // detail对象，提供给事件监听函数
+      let myEventOption = {
+        'bubbles': false, // 事件是否冒泡
+        'composed': false, // 事件是否可以穿越组件边界，为false时，事件将只能在引用组件的节点树上触发，不进入其他任何组件内部
+        'capturePhase': false, // 事件是否拥有捕获阶段
+      }; // 触发事件的选项
+      this.triggerEvent(callBackFunctionName, myEventDetail, myEventOption);
+    },
+
+    /**
+     * 月份选择
+     */
+    tapMonthSelecter: function (e) {
+      let type = e.currentTarget.dataset.type;
+      if (type == 'last') {
+        this._setCurrentPageDate(-1);
+      } else {
+        this._setCurrentPageDate(1);
+      }
+    }
   },
 
   /** ==================================================== */
@@ -76,9 +160,9 @@ Component({
   created: function () {
     // 在组件实例刚刚被创建时执行
     this.setData({
-      currentDate: timeUtils.getDate(new Date(), 0, 0, 0, 0, 0,0,0)
+      today: new Date()
     })
-    this._getStartAndEndDayWeek();
+    this._setCurrentPageDate(0);
   },
   attached: function () {
     // 在组件实例进入页面节点树时执行
@@ -121,7 +205,7 @@ Component({
   /**
    * 外部样式类
    */
-  externalClasses: ["i-class"],
+  externalClasses: ["i-class", "i-week-title-class", "i-day-item-empty-class", "i-day-item-class", "i-day-item-title-class", "i-day-item-lighting-class"],
 
   /** ==================================================== */
   /** ================= 样式 End ==================== */
--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,18 +1,98 @@
 // pages/calendar/calendar.js
+const timeUtils = require("../../lee-components/utils/timeUtils.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    lightingDayList:{}, // 点亮的时间
+  },
 
+  /**
+   * 选中时间
+   */
+  tapCalendarDay: function (e) {
+    console.log("点击时间：\n" + JSON.stringify(e))
+    let isLight = false;
+    for (let i = 0; i < this.data.lightingDayList.length; i++) {
+      let tempLightDay = this.data.lightingDayList[i];
+      if (tempLightDay.day == e.detail.currentTarget.dataset.day
+        && tempLightDay.month == e.detail.currentTarget.dataset.month
+        && tempLightDay.year == e.detail.currentTarget.dataset.year) {
+        this.data.lightingDayList.splice(i,1);
+        isLight = true;
+      }
+    }
+    if (!isLight) {
+      let tempData = {
+        day: e.detail.currentTarget.dataset.day,
+        month: e.detail.currentTarget.dataset.month,
+        year: e.detail.currentTarget.dataset.year,
+        lightingType: "remind",
+      }
+      this.data.lightingDayList.push(tempData);
+    }
+
+    this.setData({
+      lightingDayList: this.data.lightingDayList
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let tempDate1 = timeUtils.getDate(new Date(), 0, 0, 0, 1, 0, 0, 0);
+    let tempDate2 = timeUtils.getDate(new Date(), 0, 0, 1, 0, 0, 0, 0);
+    let tempDate3 = timeUtils.getDate(new Date(), 0, 0, 0, 3, 0, 0, 0);
+    let tempDate4 = timeUtils.getDate(new Date(), 0, 0, 0, 5, 0, 0, 0);
+    let tempDate5 = timeUtils.getDate(new Date(), 0, 1, 0, 0, 0, 0, 0);
+    let tempDate6 = timeUtils.getDate(new Date(), 0, 0, 0, 8, 0, 0, 0);
 
+    // warning 红色 | alert 黄色 | remind 蓝色 | complete 绿色 | default 灰色
+    let tempData1 = {
+      day: tempDate1.getDate(),
+      month: tempDate1.getMonth() + 1,
+      year: tempDate1.getFullYear(),
+      lightingType: "warning",
+    };
+    let tempData2 = {
+      day: tempDate2.getDate(),
+      month: tempDate2.getMonth() + 1,
+      year: tempDate2.getFullYear(),
+      lightingType: "default",
+    };
+    let tempData3 = {
+      day: tempDate3.getDate(),
+      month: tempDate3.getMonth() + 1,
+      year: tempDate3.getFullYear(),
+      lightingType: "remind",
+    };
+    let tempData4 = {
+      day: tempDate4.getDate(),
+      month: tempDate4.getMonth() + 1,
+      year: tempDate4.getFullYear(),
+      lightingType: "complete",
+    };
+    let tempData5 = {
+      day: tempDate5.getDate(),
+      month: tempDate5.getMonth() + 1,
+      year: tempDate5.getFullYear(),
+      lightingType: "alert",
+    };
+    let tempData6 = {
+      day: tempDate6.getDate(),
+      month: tempDate6.getMonth() + 1,
+      year: tempDate6.getFullYear(),
+      lightingType: "alert",
+    };
+    let tempList = [];
+    tempList = [tempData1, tempData2, tempData3, tempData4, tempData5, tempData6];
+
+    this.setData({
+      lightingDayList: tempList
+    })
   },
 
   /**
