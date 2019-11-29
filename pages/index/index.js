ed4b367514e8eadeb17c13ad5f5a49f069a6ce8e
Jingbo@li <18811145767@163.com>
2019-11-29 13:36:26 +0800

    201911291336
    进度条 leeProgress

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,45 +8,48 @@ Page({
     dataSource: [
       {
         name: "倒计时 -- leeCountDown",
-        target: "../countDown/countDown"
+        target: "/pages/countDown/countDown"
       },
       {
         name: "时间展示 -- leeShowTime",
-        target: "../showTime/showTime"
+        target: "/pages/showTime/showTime"
       },
       {
         name: "日历 -- leeCalendar",
-        target: "../calendar/calendar"
+        target: "/pages/calendar/calendar"
       },
       {
         name: "图标 -- leeIcon",
-        target: "../icon/icon"
+        target: "/pages/icon/icon"
       },
       {
-        name: "通知 | 公告 -- leeNotice",
-        target: "../notice/notice"
+        name: "通知 | 公告 -- leeNotice(未)",
+        target: "/pages/notice/notice"
       },
       {
-        name: "获取验证码按钮 -- leeGetVerifiCodeButton",
+        name: "获取验证码按钮 -- leeGetVerifiCodeButton(未)",
         target: ""
       },
       {
         name: "自定义NavigationBar -- leeNavigationBar",
-        target: "../navigationBar/navigationBar"
+        target: "/pages/navigationBar/navigationBar"
       },
       {
-        name: "电话 -- leePhoneCall",
-        target: "../phoneCallButton/phoneCallButton"
+        name: "电话 -- leePhoneCall(未)",
+        target: "/pages/phoneCallButton/phoneCallButton"
       },
       {
         name: "带占位图Image -- leePlaceholdImage",
-        target: "../placeholdImage/placeholdImage"
+        target: "/pages/placeholdImage/placeholdImage"
       },
       {
         name: "loadmore底部item -- leeLoadingFootItem",
-        target: "../loadingFootItem/loadingFootItem"
+        target: "/pages/loadingFootItem/loadingFootItem"
       },
-
+      {
+        name: "进度条 -- leeProgress",
+        target: "/pages/progress/progress"
+      },
     ],
   },
   
